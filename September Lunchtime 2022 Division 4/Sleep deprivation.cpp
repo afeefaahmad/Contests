5d522821ee27f712ff9674125f770461008fb224
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2022-09-23 22:02:24 +0530

    https://www.codechef.com/LTIME112D/problems/SLEEP

---
+++ September Lunchtime 2022 Division 4/Sleep deprivation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,sleep;
+	cin >>t;
+	while(t--) {
+	    cin >>sleep;
+	    if(sleep <7)    cout<<"yes"<<endl;
+	    else    cout<<"no"<<endl;
+	    
+	}
+	return 0;
+}
